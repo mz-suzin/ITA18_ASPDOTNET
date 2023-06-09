b3c538865b52d2fed5d411281eea7ea40551d44c
Suzin <matheus.zimmermannsuzin@dellteam.com>
2023-06-09 16:51:37 -0300

    sei lá, nao consegui mais acompanhar. Professor pressupoe que a gente é o mestre de C#, nao to acompanhando

--- EntityFramework/DemoEFWebApi/Program.cs
+++ EntityFramework/DemoEFWebApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using DemoEFWebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LojinhaContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ EntityFramework/DemoEFWebApi/README.txt
@@ -0,0 +1,5 @@
+para criar os migrations no cmd:
+dotnet ef migrations add Inicial
+
+para mandar gerar na database as migrations:
+dotnet ef database update
